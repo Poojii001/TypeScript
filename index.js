b1eb472d30d05f93eaccb47ba63298ad064ef71c
Poojii001 <poojapal5781@gmail.com>
2025-10-30 19:13:36 +0530

    TypeScript

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-console.log("Hello world1");
-console.log("Hello world2");
-console.log("Hello world3");
-console.log("Hello world4");
-console.log("Hello world5");
+// console.log("Hello world1")
+// console.log("Hello world2")
+// console.log("Hello world3")
+// console.log("Hello world4")
+// console.log("Hello world5")
+//type annotations
+// var a:number = 10
+// console.log(a)
+// a = "Pooja"
+// console.log(a)
+// a = true
+// console.log(a)
+//type interface
+// var a = 10
+// console.log(a)
+// a = "Pooja"
+// console.log(a)
+// a = true
+// console.log(a)
+//any type
+var a = 10;
+console.log(a);
+a = "Pooja";
+console.log(a);
+a = true;
+console.log(a);
