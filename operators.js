705aab2c51f12739a9aa2a6385c378afe6ed96ce
Poojii001 <poojapal5781@gmail.com>
2025-11-01 13:04:51 +0530

    operators

--- operators.js
+++ operators.js
@@ -1,17 +1,23 @@
 //Arithmatic Operators
-console.log(15 + 6);
-console.log(15 + "6");
-console.log("15" + "6");
-console.log(15 + "Hello world");
-console.log(15 - 6); //9
-console.log(15 - "6"); //error
-console.log("15" - "6"); //error
-console.log(15 - "Hello world"); //error
-console.log(15 * 6); //9
-console.log(15 * "6"); //error
-console.log("15" - "6"); //error
-console.log(15 * "Hello world"); //error
-console.log(15 / 6); //9
-console.log(15 / "6"); //error
-console.log("15" / "6"); //error
-console.log(15 / "Hello world"); //error
+// console.log(15+6)
+// console.log(15+"6")
+// console.log("15"+"6")
+// console.log(15+ "Hello world")
+// console.log(15-6)  //9
+// console.log(15-"6")  //error
+// console.log("15"-"6")   //error
+// console.log(15- "Hello world")   //error
+// console.log(15*6)  //9
+// console.log(15*"6")  //error
+// console.log("15"-"6")   //error
+// console.log(15* "Hello world")   //error
+// console.log(15/6)  //9
+// console.log(15/"6")  //error
+// console.log("15"/"6")   //error
+// console.log(15/ "Hello world")   //error
+//relation
+console.log(10 < 20); //t
+console.log(10 > 20); //f
+console.log(10 < "20"); //error
+console.log("10" < 20); //eror
+console.log("10" < "20"); //f
