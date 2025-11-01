ce9ca065acbce789f9abce7d73f154bb139a776e
Poojii001 <poojapal5781@gmail.com>
2025-11-01 13:00:36 +0530

    operators

---
+++ operators.ts
@@ -0,0 +1,22 @@
+//Arithmatic Operators
+console.log(15+6)
+console.log(15+"6")
+console.log("15"+"6")
+console.log(15+ "Hello world")
+
+ 
+console.log(15-6)  //9
+console.log(15-"6")  //error
+console.log("15"-"6")   //error
+console.log(15- "Hello world")   //error
+
+
+console.log(15*6)  //9
+console.log(15*"6")  //error
+console.log("15"-"6")   //error
+console.log(15* "Hello world")   //error
+
+console.log(15/6)  //9
+console.log(15/"6")  //error
+console.log("15"/"6")   //error
+console.log(15/ "Hello world")   //error
