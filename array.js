ed19cd3f426873725f5ea8333b2a84a161e7b774
Poojii001 <poojapal5781@gmail.com>
2025-11-01 14:06:19 +0530

    array.ts

---
+++ array.js
@@ -0,0 +1,6 @@
+var a = [10, 20, 30, 40, 50, "Pooja", "Raj", "Akhil", "Hema", true, false, [1, 2, 3, 4, 5]];
+var a = [10, 20, 30, 40, 50, "Pooja", "Raj", "Akhil", "Hema", true, false, [1, 2, 3, 4, 5]];
+var a = [10, 20, 30, 40, 50, "Pooja", "Raj", "Akhil", "Hema", true, false, [1, 2, 3, 4, 5]];
+var a = [10, 20, 30, 40, 50, "Pooja", "Raj", "Akhil", "Hema", true, false, [1, 2, 3, 4, 5]];
+var a = [10, 20, 30, 40, 50, "Pooja", "Raj", "Akhil", "Hema", true, false, [1, 2, 3, 4, 5]];
+var a = [10, 20, 30, 40, 50, "Pooja", "Raj", "Akhil", "Hema", true, false, [1, 2, 3, 4, 5]];
