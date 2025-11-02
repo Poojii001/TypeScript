eb90fc5fa13902bcb60aef29682e2b3c1e9533ca
Poojii001 <poojapal5781@gmail.com>
2025-11-02 14:30:58 +0530

    abstraction.ts

---
+++ abstraction.ts
@@ -0,0 +1,17 @@
+class Test {
+    public a:number =10
+    protected b:number =20
+    private c:number =30
+    display() {
+        console.log(`In display() of test class a =${this.a} b=${this.b} c=${this.c}`)
+    }
+}
+class sample extends Test {
+    show() {
+        console.log(`In show() of test class a =${this.a} b=${this.b} c =${this.c}`)
+    }
+}
+var obj = new sample()
+obj.display()
+obj.show()
+console.log(`In main scope a = ${obj.a} b={obj.b} c=${obj.c}`)
